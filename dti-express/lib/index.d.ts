d937c7190712f22e8c286179077ff7fbce7538d2
farcek <farcek@gmail.com>
2019-12-30 22:48:38 +0800

    del

--- dti-express/lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { RestAPI } from "@napp/dti";
-export declare class ServerHandle<RQ, RS> {
-    private Cls;
-    constructor(Cls: RestAPI<RQ, RS>);
-    action(handle: (rq: RQ) => Promise<RS>): void;
-}
-export declare function factory<RQ, RS>(Cls: RestAPI<RQ, RS>): ServerHandle<RQ, RS>;
